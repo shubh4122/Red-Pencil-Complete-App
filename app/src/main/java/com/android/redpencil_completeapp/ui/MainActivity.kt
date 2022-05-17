15a37f7a18eb78ac6c4f6c2820fda959323e35e2
Shubham Pandey <shubhampandeylearning@gmail.com>
2022-05-18 01:12:30 +0530

    Initial Commit

---
+++ app/src/main/java/com/android/redpencil_completeapp/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.android.redpencil_completeapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.android.redpencil_completeapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
