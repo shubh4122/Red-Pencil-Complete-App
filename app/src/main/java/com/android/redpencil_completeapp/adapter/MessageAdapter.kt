3b2d3d18b564d5706e24f69607f0cd3057c193f1
Shubham Pandey <shubhampandeylearning@gmail.com>
2022-05-19 20:20:22 +0530

    Added Floating Context menu

--- app/src/main/java/com/android/redpencil_completeapp/adapter/MessageAdapter.kt
+++ app/src/main/java/com/android/redpencil_completeapp/adapter/MessageAdapter.kt
@@ -1,12 +1,11 @@
 package com.android.redpencil_completeapp.adapter
 
-import android.util.Log
+import android.view.ContextMenu
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.android.redpencil_completeapp.R
 import com.android.redpencil_completeapp.models.Message
@@ -14,11 +13,26 @@ import com.bumptech.glide.Glide
 
 class MessageAdapter(private val messageList : ArrayList<Message>) :
     RecyclerView.Adapter<MessageAdapter.MessageViewHolder>() {
-    inner class MessageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+
+    inner class MessageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnCreateContextMenuListener {
         val senderNameTextView : TextView = itemView.findViewById(R.id.senderName)
         val messageTextView : TextView = itemView.findViewById(R.id.message)
         val timeTextView : TextView = itemView.findViewById(R.id.time)
         val messageImageView : ImageView = itemView.findViewById(R.id.messageImage)
+
+        init {
+            itemView.setOnCreateContextMenuListener(this)
+        }
+
+        override fun onCreateContextMenu(
+            menu: ContextMenu?,
+            v: View?,
+            menuInfo: ContextMenu.ContextMenuInfo?
+        ) {
+            menu?.add(adapterPosition, 101, 0, "Edit")
+            menu?.add(adapterPosition, 102, 1, "Delete")
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MessageViewHolder {
